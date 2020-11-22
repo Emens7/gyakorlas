dd40cb213184d02a4484da49d79c41769b7018f5
Magyar István <magyaristvanlaszlo@gmail.com>
2020-11-22 17:18:44 +0100

    Teljes

---
+++ HatterszinValto/css/js/js.js
@@ -0,0 +1,16 @@
+function colorGray() {
+    document.body.style.backgroundColor = "rgb(163, 150, 150)";
+}
+
+function colorWhite() {
+    document.body.style.backgroundColor = "rgb(250, 246, 246)";
+}
+
+function colorBlue() {
+    document.body.style.backgroundColor = "rgb(17, 21, 223)";
+}
+
+function colorYellow() {
+    document.body.style.backgroundColor = "rgb(230, 216, 24)";
+}
+
