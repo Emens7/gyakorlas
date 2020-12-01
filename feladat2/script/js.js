ab5bffea596b30e91df2b5fe3e1a86c4cf4bd8f8
István Magyar <magyaristvanlaszlo@gmail.com>
2020-12-01 22:47:23 +0100

    első conflict

--- feladat2/script/js.js
+++
@@ -1,7 +0,0 @@
-const summation = (a = 0, b = 0) => a + b; 
-
-const subraction = (a = 0, b = 0) => a -b ;
-
-console.log(summation(5, 5));
-
-console.log(subraction (100, 50));
--- feladat3/script/js.js
+++
@@ -1,6 +0,0 @@
-const personDatalog = ( { firstname = 'John', lastname = 'Doe', age = 33 } ) => {
-    
-    console.log(`My name is ${firstname} ${lastname}. I'm ${age} years old.`)
-}
-
-personDatalog({firstname:'Ede', lastname: 'Szasz', age: 28});
